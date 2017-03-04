04129c95172061fb285ffa6ea55341707a68313e
Ej-PC\Ej <eliazersantos.mail@gmail.com>
2017-03-04 20:54:06 +0800

    method overloading code

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -3,6 +3,31 @@ package com.company;
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+        // write your code here
+        System.out.println(calcFeetAndInchesToCentimeters(-1));
     }
+
+    public static double calcFeetAndInchesToCentimeters(double feet, double inches) {
+        double feetToCentimeter;
+        double inchToCentimeter;
+        if (feet >= 0 && inches >= 0 && inches <= 12) {
+            feetToCentimeter = feet * 30.48;
+            inchToCentimeter = inches * 2.54;
+            return feetToCentimeter + inchToCentimeter;
+        } else {
+            return -1;
+        }
+    }
+
+
+    public static double calcFeetAndInchesToCentimeters(double inches) {
+        double feet;
+        if (inches >= 0) {
+            feet = inches / 12;
+            return calcFeetAndInchesToCentimeters(feet, 0);
+        } else {
+            return -1;
+        }
+    }
+
 }
